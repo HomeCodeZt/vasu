2ae42bb2bf0c87d1b4e1ece35d25bb86c7073dc9
alex <alexandr_r@i.ua>
2017-09-12 16:09:10 +0300

    Новый стиль

---
+++ web/public/js/script.js
@@ -0,0 +1,26 @@
+function clock() {
+var d = new Date();
+var month_num = d.getMonth()
+var day = d.getDate();
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var seconds = d.getSeconds();
+ 
+month=new Array("січня", "лютого", "березня", "квітня", "травня", "червня",
+"липня", "серпня", "вересня", "жовтня", "листопада", "грудня");
+ 
+if (day <= 9) day = "0" + day;
+if (hours <= 9) hours = "0" + hours;
+if (minutes <= 9) minutes = "0" + minutes;
+if (seconds <= 9) seconds = "0" + seconds;
+ 
+ date_time ="<div class='time'>"+ hours + ":" + minutes + ":" + seconds + "<br>" + day + " " + month[month_num] + " " + d.getFullYear() +
+"р.</div>";
+
+if (document.layers) {
+ document.layers.doc_time.document.write(date_time);
+ document.layers.doc_time.document.close();
+}
+else document.getElementById("doc_time").innerHTML = date_time;
+ setTimeout("clock()", 1000);
+}
